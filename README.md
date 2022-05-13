ae1be89891272d49e6b3f67a6055b4bcbaec69de
Bilal K <lalbikhan2014@gmail.com>
2022-05-13 16:42:48 +0500

    users route

---
+++ README.md
@@ -0,0 +1,3 @@
+Routes.
+GET /users find all users
+GET /users/:id find user details
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoute from './routes/users.js'
+
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json()); //using json for the whole application
+
+app.use('/users',usersRoute);
+
 app.get('/',(req,res)=>{
     console.log('[TEST]!')
     res.send('hello from the homepage')
