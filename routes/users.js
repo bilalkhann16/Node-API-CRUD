12f361b0ec0779260eb8a29e2b58234e7c24d69a
Bilal K <lalbikhan2014@gmail.com>
2022-05-22 17:46:59 +0500

    new users post request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 import express from "express";
 const router = express.Router();
 
-router.get('/users',(req,res)=>{
-    res.send("Hello from users.js");
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 30,
+    },
+    {
+        firstName: "Bilal",
+        lastName: "K.",
+        age:21
+    }
+]
+// all routes in here are starting with /users
+router.get('/',(req,res)=>{
+    console.log(users);
+    res.send(users);
 });
 
+router.post('/',(req,res)=>{
+    console.log("post request created");
+    res.send("post request created")
+});
+ 
 
 export default router;
